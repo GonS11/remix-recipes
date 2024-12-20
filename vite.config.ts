6448ec2ca3de657c39b722820d5249802dad75c0
“Gonzalo” <gonzalos.camara@gmail.com>
2024-12-20 17:38:58 +0100

    Inicio y configuracion Proyecto Remix-Recipes

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,22 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
+import { createRoutesFromFolders } from "@remix-run/v1-route-convention";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
+installGlobals();
 
 export default defineConfig({
   plugins: [
     remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
+      ignoredRouteFiles:["**/.*"],
+      routes(defineRoutes){
+        return createRoutesFromFolders(defineRoutes);
       },
     }),
     tsconfigPaths(),
   ],
+  server:{
+    port:3000,
+  }
 });
