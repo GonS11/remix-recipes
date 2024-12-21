d6be42ae1f208cc834aa2ed49d68cf97d41491f1
“Gonzalo” <gonzalos.camara@gmail.com>
2024-12-21 13:20:09 +0100

    Redireccion /app /app/pantry

--- app/routes/app/index.tsx
+++ app/routes/app/index.tsx
@@ -1,6 +1,12 @@
-import { LoaderFunction } from '@remix-run/node';
+import { LoaderFunction, redirect } from '@remix-run/node';
 
-//En este caso esta pagina es un index de la ruta app, pero como queremos que siempre vaya a app/pantry directamente, solo se despliega con app solo. Pero como no queremos mostrar nada en este index no hace falta crear un componente, sino que funcione como carga de recursos como un loader. Si se hace un loader normal con return devuelve un json. Pero para que no nos salga eso no hace falta deovlver nada
+//En este caso esta pagina es un index de la ruta app, pero como queremos que siempre vaya a app/pantry directamente, solo se despliega con app solo. Pero como no queremos mostrar nada en este index no hace falta crear un componente, sino que funcione como carga de recursos como un loader. Si se hace un loader normal con return devuelve un json. Pero para que no nos salga eso no hace falta devolver nada, para eso le pasammos un codigo 3xx/redireccion
 export const loader: LoaderFunction = () => {
-  return 'hello';
+  /* return new Response(null, {
+    status: 302,
+    headers: { Location: '/app/pantry' },
+  }); */
+
+  //Simplificado
+  return redirect('/app/pantry');
 };
